0dc9924abf6f800a01090971b72d49453c293a62
Wesley Jones <wes.jones@me.com>
2023-03-06 12:39:00 -0500

    add main header to new page

--- frontend/src/DestSelector/DestSelector.js
+++ frontend/src/DestSelector/DestSelector.js
@@ -1,7 +1,12 @@
 import './DestSelector.css'
+import MainHeader from '../MainHeader/MainHeader'
 
 function DestSelector() {
-    
+    return (
+        <div>
+            <MainHeader />
+        </div>
+    )
 }
 
 export default DestSelector
