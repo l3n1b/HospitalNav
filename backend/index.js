656ba94f51e9ace21385ab22bbbbe86e1bc6bb4b
Leni <83179073+l3n1b@users.noreply.github.com>
2023-03-26 20:12:05 -0400

    adding routes to test

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,39 @@ To test if a route works, go http://localhost:3000/???? Ex: http://localhost:300
 */
 const express = require('express')
 const app = express()
+const router = new express.Router();
+
+
+/* for session things  */
+const uuid = require('uuid')
+const session = require('express-session');
+
+app.use(session({
+    genid: uuid.v4,
+    name: 'refreshy',
+    secret: 'supersecret',
+    proxy: false,
+    resave: false,
+    rolling: true, //don't know if this needs to be true or false
+    saveUninitialized: false,
+    cookie:{
+        path: '/',
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: false
+    }
+}))
+
+app.use(router)
+
+/*                     */
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("This is the root!")
-})
+// app.get("/", (req, res) => {
+//     res.send("This is the root!")
+// })
 
 app.get("/helloworld", (req, res) => {
     res.send("Hello World!")
@@ -23,3 +49,51 @@ app.listen(port, err => {
     }
     console.log(`Listening on port ${port}`);
 });
+
+
+
+router.route('/')
+    .get((req, res)=>{
+        res.send(`This is the root!`);
+    })
+    .post((req, res)=>{
+        res.send("I'm on post for /");
+    })
+
+router.route('/start/:startID')
+    .get((req, res)=>{
+        res.send(`GET: Requesting Resource "${req.params.startID}" from /start`);
+    })
+    .put((req, res)=>{
+        res.send(`PUT: Requesting Resource "${req.params.startID}" from /start`);
+    })
+
+router.route('/route/:startID/:destID')
+    .get((req, res)=>{
+        res.send(`GET: Requesting Resource start:"${req.params.startID}" and dest: "${req.params.destID}" from /route`);
+    })
+    .put((req, res)=>{
+        res.send(`PUT: Requesting Resource start:"${req.params.startID}" and dest: "${req.params.destID}" from /route`);
+    })
+
+router.route('/destination/:destID')
+    .get((req, res)=>{
+        res.send(`GET: Requesting Resource "${req.params.destID}" from /destination`);
+    })
+    .put((req, res)=>{
+        res.send(`PUT: Requesting Resource "${req.params.destID}" from /destination`);
+    })
+
+//send image to frontend
+
+
+
+// router
+//     .route("/cars/:carid")
+//     .get((req, res) => {
+//       res.send("hi get /things/cars/" + req.params.carid);
+//     })
+//     .put((req, res) => {
+//       res.send("hi put /things/cars/" + req.params.carid);
+//     });
+  
