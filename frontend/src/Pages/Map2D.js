ccf09637bcaf3de6efe089a95e68cc49e71742d2
Joseph Lindemuth <joe@uky.edu>
2023-03-28 18:04:26 -0400

    Made interactive map package work

--- frontend/src/Pages/Map2D.js
+++ frontend/src/Pages/Map2D.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import MainHeader from '../Partials/MainHeader';
+import { Viewer } from '@photo-sphere-viewer/core';
+import { ReactPhotoSphereViewer, MapPlugin, CompassPlugin } from 'react-photo-sphere-viewer';
+import './Map2d.css'
 
 function Map2D() {
     let {start, end} = useParams()
+
+    const plugins =  [
+            [MapPlugin, {
+                imageUrl: (process.env.PUBLIC_URL + '/maps/Floor1.png'),
+                center: { x: 785, y: 421 },
+                rotation: '-12deg',
+            }],
+        ];
+
+    // const plugins2 = [
+    //     [CompassPlugin, {
+    //         hotspots: [
+    //         { longitude: '0deg' },
+    //         { longitude: '90deg' },
+    //         { longitude: '180deg' },
+    //         { longitude: '270deg' },
+    //         ],
+    //     }]
+    // ]
 
     return (
         <div className='Map2D'>
@@ -14,8 +36,34 @@ function Map2D() {
                 <p><a href='..'>Choose new start location</a></p>
                 <p><a href={'../' + start}>Choose new end location</a></p>
             </div>
+
+            {/* <img src={process.env.PUBLIC_URL + '/images/Hall1.JPG'} className="testImg"></img> */}
+
+            {/* const viewer = new PhotoSphereViewer.Viewer({
+            plugins: [
+                [PhotoSphereViewer.MapPlugin, {
+                    imageUrl: 'path/to/map.jpg',
+                    center: { x: 785, y: 421 },
+                    rotation: '-12deg',
+                }],
+            ],
+        }); */}
+
+            <div id="viewer"></div>
+            <ReactPhotoSphereViewer src={process.env.PUBLIC_URL + '/images/Hall1.JPG'}
+                height={'80vh'} width={"100%"} plugins={plugins}
+                ></ReactPhotoSphereViewer>
+            {photoViewer()}
 		</div>
     )
+}
+
+function photoViewer() {
+    console.log("photo viewer")
+    // const viewer = new Viewer({
+    //     container: document.querySelector('#viewer'),
+    //     panorama: (process.env.PUBLIC_URL + '/images/Hall1.JPG'),
+    // });
 }
 
 export default Map2D;
