3a085c6d77fe12506049a7272a5b68d885f0aa95
Wesley Jones <wes.jones@me.com>
2023-04-03 13:16:30 -0400

    create basic sampler test

--- frontend/cypress/e2e/homepage.cy.js
+++ frontend/cypress/e2e/homepage.cy.js
@@ -1,5 +1,15 @@
 describe('Visit homepage', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('localhost:3000/')
+    
+    // set up an intercept for image grab
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/data/*',
+      },
+      // This is where we put what we want the response to look like
+      []
+    ).as('getData') // and assign an alias
   })
 })
