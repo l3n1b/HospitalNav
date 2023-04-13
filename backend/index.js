f5694d37d2162f0f36bc603f4ea7552370732f1b
Joseph Lindemuth <jlindemuth@opentext.com>
2023-04-13 13:24:53 -0400

    Added disjkstra's algorithm to calculate shortest route based on connections

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,6 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     }
-
     next();
 });
 
@@ -120,21 +119,79 @@ router.route('/data/:startID/:endID')
 .get((req, res) => {
     let start = req.params.startID;
     let end = req.params.endID;
-    x_coord = (dataJson[start]['x_coord']*2.75846)+309.9
-    y_coord = (dataJson[start]['y_coord']*2.94737)+462.8
+    x_coord = (dataJson[start]['x_coord']*2.75846)+309.9;
+    y_coord = (dataJson[start]['y_coord']*2.94737)+462.8;
     image_path = "/images/"+start+".JPG";
 
-    navigation_route = getRoute(start, end)
+    navigation_route = getRoute(start, end);
 
-    res.send({x: x_coord, y: y_coord, imagePath: image_path, route: navigation_route})
+    res.send({x: x_coord, y: y_coord, imagePath: image_path, route: navigation_route});
 })
 
-function getRoute(start, end) {
-    return [{'name': '1st Floor Main Entrance', 'x_coord': 250.0, 'y_coord': 98.0, 'floor': '1', 'angle': -0.8746, 'angle_back': 2.266992653589793, 'string_direction': 'Head straight for 15 feet', 'line_name': 'line_1st Floor Main Entrance_Hall4_Vascular Interventional Radiology.png'},
-    {'name': 'Hall4', 'x_coord': 264.0, 'y_coord': 88.0, 'floor': '1', 'angle_back': 0.063, 'angle': 3.0, 'string_direction': 'Head straight for 25 feet', 'line_name': 'line_Hall4_Vascular Interventional Radiology.png'},
-    {'name': 'Vascular Interventional Radiology', 'x_coord': 289.0, 'y_coord': 86.0, 'floor': '1', 'angle_back': -0.8155, 'angle': 2.326092653589793, 'string_direction': "You've arrived!", 'line_name': 'line_Vascular Interventional Radiology.png'}]
-}
+// Shoutout to chat GPT for the layout and comments of this function
+// Diskstra's algorithm for finding the shortest path
+// Applied to KYCTestValues.json looking at the connectionsList of each location
+function getRoute(startNode, endNode) {
+    const distances = {};
+    const visited = {};
+    const previous = {};
+    const queue = [];
 
+    // Initialize all distances to infinity, and the startNode's distance to 0
+    for (let node in dataJson) {
+      distances[node] = Infinity;
+    }
+    distances[startNode] = 0;
+
+    // Add the startNode to the queue
+    queue.push(startNode);
+
+    // Loop until the queue is empty
+    while (queue.length > 0) {
+      // Get the node with the shortest distance from the startNode
+      let currentNode = queue.shift();
+
+      // If this is the endNode, we're done!
+      if (currentNode === endNode) {
+        // Build the path by traversing the previous object
+        const path = [];
+        while (currentNode) {
+          path.push(currentNode);
+          currentNode = previous[currentNode];
+        }
+        path.reverse();
+        return path;
+      }
+
+      // Skip this node if we've already visited it
+      if (visited[currentNode]) {
+        continue;
+      }
+      visited[currentNode] = true;
+
+      // Update the distances of this node's neighbors
+      for (let neighbor of getConnections(currentNode)) {
+        let distance = 1;
+        let totalDistance = distance + distances[currentNode];
+        if (totalDistance < distances[neighbor]) {
+          distances[neighbor] = totalDistance;
+          previous[neighbor] = currentNode;
+          queue.push(neighbor);
+        }
+      }
+    }
+
+    // If we get here, there's no path from startNode to endNode
+    return null;
+  }
+
+function getConnections(location) {
+    let connections = [];
+    for(let connection of dataJson[location]['connectList']){
+        connections.push(connection.name);
+    }
+    return connections
+}
 
 // router
 //     .route("/cars/:carid")
